16a47ef3a4654ad849d75c46365be95af325938d
Walyn <walyncurtis@gmail.com>
2023-11-05 11:41:01 -0600

    Revert "Continues work on visual themes, menu scene now loads a copy of main_menu"
    This reverts commit 66e9eb6d461d49d290f06736e274e8ce6f48e8a5.

--- VisualTheme.gd
+++ VisualTheme.gd
@@ -1,45 +1,25 @@
 extends Node
 class_name VisualTheme
 
-static var themePath: PackedScene #res path to theme being used
+static var themePath: String #res path to theme being used
 static var themeInstance: VisualTheme #instance of visual theme
 
 enum SceneType {MainMenu, Game, Tutorial}
 enum UIType {MainMenu, NewGameBasic, CustomGame, Game, Pause, Win, Lose}
 
-@export var scenes: Array[PackedScene] #UI scenes that need to be instantiated, should correspond to UIType
+@export var scenes: Array[Control] #UI scenes that need to be instantiated, should correspond to UIType
 var instances: Array[Control] #instances of scenes
 
-static var currentScene: SceneType
-
-func _ready():
-	InitScene(currentScene)
-
-static func Init(tree):
-	print("Init")
-	themePath = preload("res://visualThemes/testTheme.tscn")
-	themeInstance = themePath.instantiate()
-	tree.root.add_child.call_deferred(themeInstance)
-	themeInstance.instances.resize(themeInstance.scenes.size())
-
-static func InitScene(sceneType:SceneType):
-	print("InitScene")
+static func InitTheme(sceneType:SceneType):
 	match sceneType:
 		SceneType.MainMenu:
-			themeInstance.instances[UIType.MainMenu] = themeInstance.scenes[UIType.MainMenu].instantiate()
-			themeInstance.add_child(themeInstance.instances[UIType.MainMenu])
-			themeInstance.instances[UIType.MainMenu].position = Vector2(-600,0)
-			Enter(UIType.MainMenu)
+			print("its working")
 		_:
 			pass
 
 static func Enter(en:UIType):
 	#Make ui "en" enter view
 	match en:
-		UIType.MainMenu:
-			var tween = themeInstance.instances[en].create_tween()
-			tween.tween_property(themeInstance.instances[en], "position", Vector2(0, 0), 1)
-			pass
 		_:
 			pass
 
