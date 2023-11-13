3cd4d539805e6a02c681197e32aae639d5fd732d
Walyn <walyncurtis@gmail.com>
2023-11-12 18:06:53 -0600

    New Game button in pause menu now moves the UI back to the game screen

--- PauseMenu.gd
+++ PauseMenu.gd
@@ -8,12 +8,20 @@ extends Control
 signal new_game()
 
 func _on_new_game_pressed():
+	var tween = exit_screen()
+	tween.tween_callback(newGameSignal)
+	tween.tween_callback(gameUI.enter_screen)
+
+func newGameSignal():
 	emit_signal("new_game")
 	
 func go_to_main_menu():
 	if (gm.board.boardGenerated):
 		SaveManager.saveData.time = GameTimer.elapsed
 		SaveManager.save_game()
+	exit_screen().tween_callback(ToMenu)
+
+func ToMenu():
 	get_tree().change_scene_to_file("res://mainScenes/menu_scene.tscn")
 
 func options_pressed():
