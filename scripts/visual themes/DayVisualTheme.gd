ba156156546b65e1375b9889abfbe4153e7bcb32
Walyn <walyncurtis@gmail.com>
2023-11-23 11:37:54 -0600

    Removes code that wasn't changed from base class from inheriting scripts

--- scripts/visual themes/DayVisualTheme.gd
+++ scripts/visual themes/DayVisualTheme.gd
@@ -1,38 +1,4 @@
 extends VisualTheme
 
-func LoadVisualTheme():
-	instance = self
-	VisualLoader.instance.controlRoot.theme = theme
-	match VisualLoader.instance.scene:
-		VisualLoader.SceneType.MainMenu:
-			LoadMainMenuVisuals()
-		VisualLoader.SceneType.Game:
-			LoadGameVisuals()
-
-func UnloadVisualTheme():
-	match VisualLoader.instance.scene:
-		VisualLoader.SceneType.MainMenu:
-			UnloadMainMenuVisuals()
-		VisualLoader.SceneType.Game:
-			UnloadGameVisuals()
-
-func GetBaseColor_int(i):
-	return GetBaseColor(GameManager.instance.board.vertices[i])
-
 func GetBaseColor(vector):
 	return Color(lerpf(0, 1, invlerp(-gold, gold, vector.x)),lerpf(0, 1, invlerp(-gold, gold, vector.y)),lerpf(0, 1, invlerp(-gold, gold, vector.z)))
-
-static func invlerp(a, b, v):
-	return (v-a)/(b-a)
-
-static func won():
-	instance.emit_signal("win")
-
-static func endwon():
-	instance.emit_signal("endWon")
-
-static func lost():
-	instance.emit_signal("lose")
-
-static func endlost():
-	instance.emit_signal("endLose")
--- scripts/visual themes/NightVisualTheme.gd
+++ scripts/visual themes/NightVisualTheme.gd
@@ -4,24 +4,6 @@ extends VisualTheme
 var randomCount = 0
 var randColor = Color.RED
 
-func LoadVisualTheme():
-	instance = self
-	VisualLoader.instance.controlRoot.theme = theme
-	match VisualLoader.instance.scene:
-		VisualLoader.SceneType.MainMenu:
-			LoadMainMenuVisuals()
-		VisualLoader.SceneType.Game:
-			LoadGameVisuals()
-
-func UnloadVisualTheme():
-	match VisualLoader.instance.scene:
-		VisualLoader.SceneType.MainMenu:
-			UnloadMainMenuVisuals()
-		VisualLoader.SceneType.Game:
-			UnloadGameVisuals()
-
-func GetBaseColor_int(i):
-	return GetBaseColor(GameManager.instance.board.vertices[i])
 
 func GetBaseColor(vector):
 	if (randomCount == 0):
@@ -29,18 +11,3 @@ func GetBaseColor(vector):
 		randColor = randomColors[GameManager.visRNG.randi_range(0,randomColors.size()-1)]
 	randomCount-=1
 	return randColor
-
-static func invlerp(a, b, v):
-	return (v-a)/(b-a)
-
-static func won():
-	instance.emit_signal("win")
-
-static func endwon():
-	instance.emit_signal("endWon")
-
-static func lost():
-	instance.emit_signal("lose")
-
-static func endlost():
-	instance.emit_signal("endLose")
