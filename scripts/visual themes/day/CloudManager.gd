21e6feb6080c961d34b05d9ec28f23373334f968
Walyn <walyncurtis@gmail.com>
2023-11-27 18:45:53 -0600

    Finishes cloud visuals

--- scripts/visual themes/day/CloudManager.gd
+++ scripts/visual themes/day/CloudManager.gd
@@ -12,6 +12,7 @@ func GenerateClouds():
 	for i in range(0, clouds.size()):
 		var speed = deg_to_rad(GameManager.visRNG.randf_range(0.5, 0.25) * (-1 if GameManager.visRNG.randi_range(0,1) == 0 else 1))
 		rotDir[i] = Vector3.UP*speed
+		clouds[i].position = Vector3.ZERO
 		clouds[i].rotate_object_local(Vector3.UP, deg_to_rad(GameManager.visRNG.randf_range(-180, 180)))
 		clouds[i].rotate_object_local(Vector3.RIGHT, deg_to_rad(GameManager.visRNG.randf_range(-180, 180)))
 		clouds[i].rotate_object_local(Vector3.FORWARD, deg_to_rad(GameManager.visRNG.randf_range(-180, 180)))
