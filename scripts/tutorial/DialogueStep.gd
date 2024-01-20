c00170b4af6462d8439d3e2eb98f976f00fc7509
Walyn <walyncurtis@gmail.com>
2024-01-20 14:37:05 -0600

    Adds glow effect to dialogue button, and hides it when not on a dialogue step

--- scripts/tutorial/DialogueStep.gd
+++ scripts/tutorial/DialogueStep.gd
@@ -2,10 +2,17 @@ extends TutorialStep
 class_name DialogueStep
 
 @export var button:Button
+var anim
 
 func Begin():
+	anim = button.get_node("AnimationPlayer")
 	button.button_up.connect(End)
+	button.show()
+	anim.play("glow")
+	
 
 func End():
 	button.button_up.disconnect(End)
+	button.hide()
+	anim.stop()
 	super.End()
