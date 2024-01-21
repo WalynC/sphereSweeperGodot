35f1b83eee80fc6cd7a41d24e060a346f220e77c
Walyn <walyncurtis@gmail.com>
2024-01-21 14:03:04 -0600

    Begins work on making world pivot move on and off screen at start/end

---
+++ scripts/WorldPivotMover.gd
@@ -0,0 +1,13 @@
+extends Node3D
+class_name WorldPivotMover
+
+static var instance
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	instance = self
+	var tween = create_tween()
+	tween.tween_property(self, "position", Vector3(0,0,0),.5)
+
+func Leave():
+	var tween = create_tween()
+	tween.tween_property(self, "position", Vector3(0,10,0),.5)
--- scripts/visual themes/day/CloudManager.gd
+++ scripts/visual themes/day/CloudManager.gd
@@ -5,11 +5,11 @@ var rotDir : Array[Vector3]
 
 func GenerateClouds():
 	rotDir.resize(clouds.size())
-	reparent(GameManager.instance.worldPivot)
 	var time = SaveManager.saveData.visualTime if GameManager.loading else 0.0
 	for i in range(0, clouds.size()):
 		var speed = deg_to_rad(GameManager.visRNG.randf_range(0.5, 0.25) * (-1 if GameManager.visRNG.randi_range(0,1) == 0 else 1))
 		rotDir[i] = Vector3.UP*speed
+		clouds[i].reparent(GameManager.instance.worldPivot)
 		clouds[i].position = Vector3.ZERO
 		clouds[i].rotate_object_local(Vector3.UP, deg_to_rad(GameManager.visRNG.randf_range(-180, 180)))
 		clouds[i].rotate_object_local(Vector3.RIGHT, deg_to_rad(GameManager.visRNG.randf_range(-180, 180)))
