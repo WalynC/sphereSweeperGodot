3d7566ec9b2f1454b1d90a028d092c64038f2dfd
Walyn <walyncurtis@gmail.com>
2024-04-27 20:17:25 -0500

    SelectStep now makes confirm button glow when ready to be pressed

--- scripts/tutorial/SelectStep.gd
+++ scripts/tutorial/SelectStep.gd
@@ -6,21 +6,40 @@ class_name SelectStep
 @export var textVariants: Array[String]
 
 var moves : Array[int]
+var playingAnim = false
+var anim
+var active = false
 
 func Begin():
+	active = true
 	controls.allowSelect = true
 	controls.selectStep = self
 	moves = movesNeeded.duplicate()
+	anim = GameUI.instance.confirmButton.get_node("AnimationPlayer")
 	#turn on button glow if needed
 	for i in moves:
 		TutorialIndicator.tutorialInst.Indicate(GameManager.instance.board.triangles[i])
+
+func _process(delta):
+	if (!active): return
+	if (controls.triangleHit >= 0):
+		if (!playingAnim):
+			print("should be glowing")
+			anim.play("glow")
+			playingAnim = true
+	else:
+		anim.stop()
+		playingAnim = false
 
 func Check():
 	if moves.size() == 0: End()
 
 func End():
+	active = false
 	controls.allowSelect = false
 	controls.selectStep = null
+	playingAnim = false
+	anim.stop()
 	#activate timer if timer is not on
 	#turn off glow if needed
 	TutorialIndicator.tutorialInst.EndIndicate()
