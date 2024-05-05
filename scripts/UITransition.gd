131adb502634a32fb97ecec02cdf07faf9800eaa
Walyn <walyncurtis@gmail.com>
2024-05-05 09:50:43 -0500

    UI is now blocked from being interacted with when a UI transition is in progress

--- scripts/UITransition.gd
+++ scripts/UITransition.gd
@@ -1,8 +1,13 @@
 extends Control
 class_name UITransition
 @export var control : Control
+var blocker : Control
+
+func _ready():
+	blocker = get_node("../../blocker")
 
 func exit_screen():
+	blocker.mouse_filter = Control.MOUSE_FILTER_STOP
 	var tween = create_tween()
 	tween.tween_property(control, "position", Vector2(0,control.get_parent_control().size.y),.5)
 	return tween
@@ -10,4 +15,8 @@ func exit_screen():
 func enter_screen():
 	var tween = create_tween()
 	tween.tween_property(control, "position", Vector2(0,0),.5)
+	tween.tween_callback(disableBlocker)
 	return tween
+
+func disableBlocker():
+	blocker.mouse_filter = Control.MOUSE_FILTER_IGNORE
