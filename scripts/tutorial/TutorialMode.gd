7452835fe5d80bb3208720703aadbb70f7b92d3b
Walyn <walyncurtis@gmail.com>
2024-08-24 10:05:59 -0500

    Fixes neighborSelect not being reset in tutorial

--- scripts/tutorial/TutorialMode.gd
+++ scripts/tutorial/TutorialMode.gd
@@ -29,6 +29,7 @@ func Reset():
 	controls.selectStep = null
 	controls.tapStep = null
 	controls.flag = false
+	controls.neighborSelect = 0
 	steps[current].Reset()
 	current = -1
 	controls.triangleHit = -999
