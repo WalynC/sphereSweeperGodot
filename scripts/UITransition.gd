679dbca1d8487ce9021782569e77897cec711479
Walyn <walyncurtis@gmail.com>
2024-09-29 16:10:18 -0500

    Fixes upper UI movement being delayed

--- scripts/UITransition.gd
+++ scripts/UITransition.gd
@@ -11,13 +11,13 @@ func exit_screen():
 	blocker.mouse_filter = Control.MOUSE_FILTER_STOP
 	var tween = create_tween()
 	tween.tween_property(control, "position", Vector2(0,control.get_parent_control().size.y),.5)
-	if (upControl):tween.tween_property(upControl, "position", Vector2(0,-upControl.get_parent_control().size.y),.5)
+	if (upControl):tween.parallel().tween_property(upControl, "position", Vector2(0,-upControl.get_parent_control().size.y),.5)
 	return tween
 
 func enter_screen():
 	var tween = create_tween()
 	tween.tween_property(control, "position", Vector2(0,0),.5)
-	if (upControl):tween.tween_property(upControl, "position", Vector2(0,0),.5)
+	if (upControl):tween.parallel().tween_property(upControl, "position", Vector2(0,0),.5)
 	tween.tween_callback(disableBlocker)
 	return tween
 
